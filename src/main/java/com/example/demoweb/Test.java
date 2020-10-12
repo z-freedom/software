bb00adb6e736f3d9d11b3ec67f3c9ea32af07968
superbigzuo <164979123@qq.com>
2020-10-12 23:14:54 +0800

    arthas-packaging-3.4.3-bin.zip

---
+++ src/main/java/com/example/demoweb/Test.java
@@ -0,0 +1,4 @@
+package com.example.demoweb;
+
+public class Test {
+}
---
+++ src/main/java/com/example/demoweb/demo/UserDto.java
@@ -0,0 +1,11 @@
+package com.example.demoweb.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class UserDto {
+    private Integer id;
+    private String name;
+}
